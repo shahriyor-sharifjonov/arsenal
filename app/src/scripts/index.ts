abe1c8fe550d674e329646bd890afba63b65ad60
Shahriyor <shahriyordeveloping@gmail.com>
2021-10-08 23:55:50 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -21,17 +21,30 @@ window.onclick = (e: MouseEvent) => {
 };
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+$(document).ready(function() {
+  $(".set > button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > button i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
     } else {
-      panel.style.display = "block";
+      $(".set > button i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > button").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
     }
   });
-}
+});
